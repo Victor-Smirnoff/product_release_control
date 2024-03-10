20e0a1887ab2895770c139c8dc180747f3bfba10
Victor Smirnov <csatom@yandex.ru>
2024-03-10 15:41:28 +0300

    feat: added __init__.py model

--- model/__init__.py
+++ model/__init__.py
@@ -2,9 +2,13 @@ __all__ = (
     "Base",
     "db_helper",
     "settings",
-
+    "ShiftTask",
+    "UniqueProductIdentifiers",
 )
+
 
 from model.database import Base
 from model.database import db_helper
 from model.config import settings
+from model.models import ShiftTask
+from model.models import UniqueProductIdentifiers
